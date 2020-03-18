1ecbecce29a7c3e55c3b05fe02da0672eb70c85d
Thiago Martins <thiago.martins@sage.com>
2020-03-17 22:17:29 -0300

    Ajustes API docker

--- backend/Api/Singleton/Database/DatabaseConfig.cs
+++ backend/Api/Singleton/Database/DatabaseConfig.cs
@@ -10,10 +10,10 @@ namespace Api.Singleton.Database
     {
         public static void Configure(IConfiguration configuration, IServiceCollection services, string server, string port, string user, string password)
         {
-            System.Console.WriteLine($"Server={server},{port};Initial Catalog=SageBackend;User Id={user};Password={password}");
+            System.Console.WriteLine($"Server={server},{port};Initial Catalog=master;User Id={user};Password={password}");
             services.AddEntityFrameworkSqlServer()
                 .AddDbContext<SageBackendContext>(
-                    options => options.UseSqlServer($"Server={server},{port};Initial Catalog=SageBackend;User Id={user};Password={password}",
+                    options => options.UseSqlServer($"Server={server},{port};Initial Catalog=master;User Id={user};Password={password}",
                         x => x.MigrationsHistoryTable("__EFMigrationsHistory", "SageBackend")));
 
             //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
